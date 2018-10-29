bed549d39219962114e37d56de127f22f30502ad
Matthew Savoca <msavoca13@gmail.com>
2018-10-29 15:21:06 +0900

    Adding weighted plots to end

--- JerES_PrelimVis.R
+++ JerES_PrelimVis.R
@@ -11,6 +11,10 @@ library(ggplot2)
 d_full <- read.csv("Odontoceti model output v9.4.csv")
 d_full$MR.exponent = as.factor(d_full$MR.exponent)
 
+#create weighted values
+d_full$Weighted_E_divesurf_max <- d_full$Percent*d_full$E_divesurf_max  #creates a column for E_divesurf_max that is weighted by Percent diet
+d_full$Weighted_E_divesurf_med <- d_full$Percent*d_full$E_divesurf_med  #creates a column for E_divesurf_med that is weighted by Percent diet
+
 
 #preliminary plots
 p1_TL_dive_med <- ggplot(d_full, aes(TL..m., E_dive_med, color = Species)) +
@@ -39,3 +43,22 @@ p1_TL_divesurf_max <- ggplot(d_full, aes(x = TL..m., y = log(E_divesurf_max), co
   geom_smooth(aes(group = MR.exponent), inherit.aes = T, method = loess) 
 
 p1_TL_divesurf_max
+
+##################
+#making figures with weighted proportions of diet
+##################
+
+p1_TL__weighted_divesurf_max <- ggplot(d_full, aes(x = TL..m., y = Weighted_E_divesurf_max, color = Species)) +
+  geom_point(inherit.aes=T) +  
+  geom_smooth(aes(group = MR.exponent), inherit.aes = T, method = loess) +
+  facet_grid(.~d_full$MR.exponent)
+
+p1_TL__weighted_divesurf_max
+
+p1_TL__weighted_divesurf_med <- ggplot(d_full, aes(x = TL..m., y = Weighted_E_divesurf_med, color = Species)) +
+  geom_point(inherit.aes=T) +  
+  geom_smooth(aes(group = MR.exponent), inherit.aes = T, method = loess) +
+  facet_grid(.~d_full$MR.exponent)
+
+p1_TL__weighted_divesurf_med
+
