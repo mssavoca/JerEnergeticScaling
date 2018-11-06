6d63c292a4a66013b67e8c02e8c3e223ae7e6e31
elhazen <ehazen@gmail.com>
2018-11-05 16:29:56 -0800

    updated with bootstrapping, and including prey weight in the model. Need to plot the weighted dive surf vs. Mass log transformed and include residuals on there.

--- JerES_PrelimVis.R
+++ JerES_PrelimVis.R
@@ -21,42 +21,73 @@ d_full$Weighted_E_divesurf_med <- d_full$Percent*d_full$E_divesurf_med  #creates
 # Simulatiing data/bootstrapping code
 ##################
 
+sd_species <- d_full %>% group_by(., Species) %>% summarise(., sd_Species=sd(E_divesurf_max))
+sd_species.med <- d_full %>% group_by(., Species) %>% summarise(., sd_Species=sd(E_divesurf_med))
+
+mean_sd_species<-mean(sd_species$sd_Species)
+mean_sd_species.med<-mean(sd_species.med$sd_Species)
+
 rn <- function(mean){
-  return(rnorm(100,mean,sd(d_full$E_divesurf_max)))
+  return(rnorm(100,mean,mean_sd_species))
+}
+
+rn.med <- function(mean){
+  return(rnorm(100,mean,mean_sd_species.med))
 }
 
 #d_new<-rnorm(100,d_full$Weighted_E_divesurf_max,sd(d_full$E_divesurf_max))
 d_new<-lapply(d_full$Weighted_E_divesurf_max,rn)
-hist(d_new[[1]],breaks=50)
+d_new.med<-lapply(d_full$Weighted_E_divesurf_med,rn.med)
+
+hist(d_new.med[[1]],breaks=50)
 
 #d_strapped<-(rep(d_full[1,],100))
 d_strapped<-d_full %>% slice(rep(1:n(), each = 100))
 
 d_strapped$Weighted_E_divesurf_max[1:100]<-d_new[[1]]
+d_strapped$Weighted_E_divesurf_med[1:100]<-d_new.med[[1]]
 
 for(i in 1:(dim(d_full)[1])){
   d_strapped$Weighted_E_divesurf_max[(100*i-99):(100*i)]<-as.integer(d_new[[i]])
+  d_strapped$Weighted_E_divesurf_med[(100*i-99):(100*i)]<-as.integer(d_new.med[[i]])
 }
-d_strapped$Weighted_E_divesurf_max[d_strapped$Weighted_E_divesurf_max<0] <- 0
 
-Eff_dive_max_gamm<- filter(d_strapped, MR.exponent == "0.61") %>% gamm(Weighted_E_divesurf_max ~ s(M..kg.,k=5), family=poisson(link='log'), random=list(Species=~1), data=.)
+d_strapped$Weighted_E_divesurf_max[d_strapped$Weighted_E_divesurf_max<0] <- 0
+d_strapped$Weighted_E_divesurf_med[d_strapped$Weighted_E_divesurf_med<0] <- 0
+
+Eff_dive_max_gamm<- filter(d_strapped, MR.exponent == "0.61") %>% gamm(Weighted_E_divesurf_max ~ s(M..kg.,k=5)+s(Prey.W..g., k=5), family=poisson(link='log'), random=list(Species=~1), data=.)
 ### $gam to look at gam effects. $lme to look at random effects.
 summary(Eff_dive_max_gamm$gam)
 plot(Eff_dive_max_gamm$gam)
 
-Eff_dive_max_gamm<- filter(d_strapped, MR.exponent == "0.68") %>% gamm(Weighted_E_divesurf_max ~ M..kg., family=poisson(link='log'), random=list(Species=~1), data=.)
+Eff_dive_max_gamm<- filter(d_strapped, MR.exponent == "0.68") %>% gamm(Weighted_E_divesurf_max ~ s(M..kg.,k=5)+s(Prey.W..g., k=5), family=poisson(link='log'), random=list(Species=~1), data=.)
 ### $gam to look at gam effects. $lme to look at random effects.
 summary(Eff_dive_max_gamm$gam)
 
-Eff_dive_max_gamm<- filter(d_strapped, MR.exponent == "0.75") %>% gamm(Weighted_E_divesurf_max ~ M..kg., family=poisson(link='log'), random=list(Species=~1), data=.)
+Eff_dive_max_gamm<- filter(d_strapped, MR.exponent == "0.75") %>% gamm(Weighted_E_divesurf_max ~ s(M..kg.,k=5)+s(Prey.W..g., k=5), family=poisson(link='log'), random=list(Species=~1), data=.)
 ### $gam to look at gam effects. $lme to look at random effects.
 summary(Eff_dive_max_gamm$gam)
+
+
+Eff_dive_med_gamm<- filter(d_strapped, MR.exponent == "0.61") %>% gamm(Weighted_E_divesurf_med ~ s(M..kg.,k=5)+s(Prey.W..g., k=5), family=poisson(link='log'), random=list(Species=~1), data=.)
+### $gam to look at gam effects. $lme to look at random effects.
+summary(Eff_dive_med_gamm$gam)
+plot(Eff_dive_med_gamm$gam)
+
+Eff_dive_med_gamm<- filter(d_strapped, MR.exponent == "0.68") %>% gamm(Weighted_E_divesurf_med ~ M..kg., family=poisson(link='log'), random=list(Species=~1), data=.)
+### $gam to look at gam effects. $lme to look at random effects.
+summary(Eff_dive_med_gamm$gam)
+
+Eff_dive_med_gamm<- filter(d_strapped, MR.exponent == "0.75") %>% gamm(Weighted_E_divesurf_med ~ M..kg., family=poisson(link='log'), random=list(Species=~1), data=.)
+### $gam to look at gam effects. $lme to look at random effects.
+summary(Eff_dive_med_gamm$gam)
+
 
 ##################
 # premliminary figures with weighted proportions of diet
 ##################
 
-p1_TL__weighted_divesurf_max <- ggplot(d_full, aes(x = TL..m., y = Weighted_E_divesurf_max, color = Species)) +
+p1_TL__weighted_divesurf_max <- ggplot(d_full, aes(x = M..kg., y = Weighted_E_divesurf_max, color = Species)) +
   geom_point(inherit.aes=T) +  
   geom_smooth(aes(group = MR.exponent), inherit.aes = T, method = loess) +
   facet_grid(.~d_full$MR.exponent)
@@ -87,7 +118,7 @@ p1_TL_dive_max <- ggplot(d_full, aes(TL..m., E_dive_max, color = Species)) +
 p1_TL_dive_max
 
 
-p1_TL_divesurf_med <- ggplot(d_full, aes(TL..m., E_divesurf_med, color = Species)) +
+p1_TL_divesurf_med <- ggplot(d_full, aes(M..kg., E_divesurf_med, color = Species)) +
   geom_point() + 
   geom_smooth(aes(group = MR.exponent), inherit.aes = T, method = loess) + 
   facet_wrap(.~d_full$MR.exponent)
@@ -95,7 +126,7 @@ p1_TL_divesurf_med <- ggplot(d_full, aes(TL..m., E_divesurf_med, color = Species
 p1_TL_divesurf_med
 
 
-p1_TL_divesurf_max <- ggplot(d_full, aes(x = TL..m., y = log(E_divesurf_max), color = Species)) +
+p1_TL_divesurf_max <- ggplot(d_full, aes(x = M..kg., y = log(E_divesurf_max), color = Species)) +
   geom_point(inherit.aes=T) + 
   facet_wrap(.~d_full$MR.exponent)  + 
   geom_smooth(aes(group = MR.exponent), inherit.aes = T, method = loess) 
