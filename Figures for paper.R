b9191ab2d2335fa98def12b06e77c6e6b61e0bf0
Matthew Savoca <matthewsavoca@DN52739u.SUNet>
2019-01-11 13:18:03 -0800

    Fig 2B working

--- Figures for paper.R
+++ Figures for paper.R
@@ -15,17 +15,25 @@ d_full <- read.csv("Cetacea model output BOUT_EXTANT.csv")
 d_sp <- read.csv("Stats by species.csv")
 
 
+
+############
 # Figure 2B
+############
+fig_2b <- ggplot(d_sp, aes(DT.max...TADL, log.value, color = Group, shape = log.of.that)) +
+  geom_point() + 
+  geom_smooth(data = filter(d_sp, Group == "Rorqual"), aes(group = log.of.that), method = lm, se = FALSE) +
+  geom_smooth(data = filter(d_sp, Group == "Odontocete"), aes(group = log.of.that), method = lm, se = FALSE) +
+  theme_bw() + guides(size=FALSE, color=FALSE) +
+  labs(x = "log[Energy  (kJ)]", y = "log[Foraging Efficiency]")
+fig_2b
 
-fig_2b <- ggplot(d_sp, aes())
 
-
+############
 # Figure 2C
-
+############
 fig_2c <- ggplot(data = d_sp, aes(DT.max...TADL, logEff_max.0.75, color = Group)) +
   geom_point() +
-  geom_smooth(data = filter(d_sp, Group == "Rorqual"), method = lm, se = FALSE) +
-  geom_smooth(data = filter(d_sp, Group == "Odontocete"), method = lm, se = FALSE) +  
+  geom_smooth(aes(group = Group), method = lm, se = FALSE) +
   theme_bw() + guides(size=FALSE, color=FALSE) +
   labs(x = "Maximum dive time - Theoretical dive time", y = "log[Foraging Efficiency]")
 fig_2c
